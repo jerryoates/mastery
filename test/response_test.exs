c699747970cb1bb6a6749e5dc04ae41b45f855b3
Jerry Oates <jerryoates1@gmail.com>
2021-08-12 14:27:05 -0400

    question test

--- test/response_test.exs
+++ test/response_test.exs
@@ -2,6 +2,20 @@ defmodule ResponseTest do
   use ExUnit.Case
   use QuizBuilders
 
+  describe "a right response and a wrong response" do
+    setup [:right, :wrong]
+
+    test "building responses checks answers", %{right: right, wrong: wrong} do
+      assert right.correct
+      refute wrong.correct
+    end
+
+    test "a timestamp is added at build time", %{right: response} do
+      assert %DateTime{ } = response.timestamp
+      assert response.timestamp < DateTime.utc_now
+    end
+  end
+
   defp quiz() do
     fields = template_fields(generators: %{left: [1], right: [2]})
 
